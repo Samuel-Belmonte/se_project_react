a93587c56362af37faf5380ac25a38869de8e8d9
SamBelmonte <sambelmonte02@gmail.com>
2024-07-08 23:23:34 -0400

    commit for project submition

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,10 +1,30 @@
 import "./WeatherCard.css";
-import sunnyDay from "../../assets/Day/sunnyDay.png";
+import { weatherOptions, defaultWeatherOptions } from "../../utils/constants";
 
 function WeatherCard({ weatherData }) {
+  const filteredOptions = weatherOptions.filter((option) => {
+    return (
+      option.day === weatherData.isDay &&
+      option.condition === weatherData.condition
+    );
+  });
+
+  let weatherOption;
+  if (filteredOptions.length === 0) {
+    weatherOption = defaultWeatherOptions[weatherData.isDay ? "day" : "night"];
+  } else {
+    const weatherOption = filteredOptions[0];
+  }
+
   return (
     <section className="weather-card">
-      <img src={sunnyDay} alt="" className="weather-card__image" />
+      <img
+        src={weatherOption?.url}
+        alt={`Card showing ${weatherOption?.condition} weather in the ${
+          weatherOption?.day ? "day" : "night"
+        }time`}
+        className="weather-card__image"
+      />
       <h2 className="weather-card__temperature">{weatherData.temp.F}&deg;F</h2>
     </section>
   );
