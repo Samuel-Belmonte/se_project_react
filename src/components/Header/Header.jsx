7d95d13a9406225db91cbd6dab5674e8f17d3708
SamBelmonte <sambelmonte02@gmail.com>
2024-07-20 22:36:36 -0400

    Add and style ToggleSwitch

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import "./Header.css";
 import logo from "../../assets/Logo.svg";
 import avatar from "../../assets/avatar.png";
+import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 
 function Header({ handleAddClick, weatherData }) {
   const currentDate = new Date().toLocaleString("default", {
@@ -14,10 +15,15 @@ function Header({ handleAddClick, weatherData }) {
       <p className="header__date-and-location">
         {currentDate}, {weatherData.city}
       </p>
-      <button onClick={handleAddClick} type="button" className="header__button">
-        + Add Clothes
-      </button>
       <div className="header__user-container">
+        <ToggleSwitch />
+        <button
+          onClick={handleAddClick}
+          type="button"
+          className="header__button"
+        >
+          + Add Clothes
+        </button>
         <p className="header__username">Terrence Tegegne</p>
         <img src={avatar} alt="Terrence Tegegne" />
       </div>
