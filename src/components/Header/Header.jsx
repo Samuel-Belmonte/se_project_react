efe92be46538233ed923292f9ffd19b274569a99
SamBelmonte <sambelmonte02@gmail.com>
2024-07-31 22:02:25 -0400

    Add link tags for navigation to and from profile section and main section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import "./Header.css";
 import logo from "../../assets/Logo.svg";
 import avatar from "../../assets/avatar.png";
@@ -11,7 +13,9 @@ function Header({ handleAddClick, weatherData }) {
 
   return (
     <header className="header">
-      <img src={logo} alt="What to Wear Logo" />
+      <Link to="/">
+        <img src={logo} alt="What to Wear Logo" />
+      </Link>
       <p className="header__date-and-location">
         {currentDate}, {weatherData.city}
       </p>
@@ -24,8 +28,12 @@ function Header({ handleAddClick, weatherData }) {
         >
           + Add Clothes
         </button>
-        <p className="header__username">Terrence Tegegne</p>
-        <img src={avatar} alt="Terrence Tegegne" />
+        <Link className="header__link" to="/profile">
+          <p className="header__username">Terrence Tegegne</p>
+        </Link>
+        <Link to="/profile">
+          <img src={avatar} alt="Terrence Tegegne" />
+        </Link>
       </div>
     </header>
   );
